dd29c6b3564cd41c95edeb8fdf25c15f806a7a07
ajin-k <41500552+ajin-k@users.noreply.github.com>
2024-07-08 19:41:51 +0530

    Bot response logic
    Task 3: create bot responses for user inputs. Static list.
    Task 4: for every user input add bot response in Chat history object.

--- chatbot/src/Componants/userInput.js
+++ chatbot/src/Componants/userInput.js
@@ -1,12 +1,20 @@
-import {useState, useEffect} from 'react'
+import { useState, useEffect, useRef } from 'react'
 
-function UserInputModule(){
+const Responses = {
+    "Hi": "Hello there, How can i help you today?",
+    "How is the weather today?": "Temp: 24 C. It might rain today",
+    "Who is the richest man in the world today?": "Elon Musk",
+    "Can you suggest me 3 best scifi books to read?": "Dune, The Forever War, Neuromancer"
+}
+
+function UserInputModule() {
     const [inputVal, setInputVal] = useState('')
     const [chatHistory, setChatHistory] = useState({})
+    var lastChatInput = useRef()
 
     //reading and setting Input field value
-    function setInput(e){
-        let val =  e.target.value;
+    function setInput(e) {
+        let val = e.target.value;
         console.log(val)
         // console.log({...chatHistory})
 
@@ -14,21 +22,60 @@ function UserInputModule(){
     }
 
     //onClick event
-    function setChatObject(){
+    function setChatObject(Entity, Messege) {
         let curruentDate = new Date(); //getting current date
-        let newObject = {} 
-        newObject[curruentDate] ={'User' : inputVal} // added key as current date and values as object
-        console.log(newObject)
-        setChatHistory({...chatHistory, ...newObject}) //using spread operator to set new value of chatHistory object
+
+        //inner object containing Entity and message
+        let newInnerObject = {}
+        newInnerObject[Entity] = Messege
+
+        // Outer object to store currentDate and innerObject
+        let newOuterObject = {}
+        newOuterObject[curruentDate] = { ...newInnerObject }
+        lastChatInput.current = curruentDate;
+        // console.log(newObject)
+        setChatHistory({ ...chatHistory, ...newOuterObject }) //using spread operator to set new value of chatHistory object
     }
 
-    //state value checking purpose.
-    useEffect(()=>{console.log("Data in chatHistory",chatHistory)},[chatHistory])
 
-     return<>
-     <input type='text' onChange={setInput} value={inputVal}></input>
-     <button onClick={setChatObject}> send</button>
-     </>
+    useEffect(() => {
+        // console.log("last Chat input", lastChatInput.current)
+        //Condition to avoid executing function on first render
+        if (lastChatInput.current != undefined) {
+            console.log("last Chat input 2", lastChatInput.current)
+            BotResponse();
+        }
+        console.log(chatHistory)
+    }, [chatHistory])
+
+    //BotResponse
+    function BotResponse() {
+        //converting date time to string since it is stored as string in object key
+        let Chatkey = lastChatInput.current.toString()
+
+        //getting all the keys in array from object inside chatHistory[key]. here only object is {'User/Bot':'input'}
+        let lastEntity = Object.keys(chatHistory[Chatkey])
+
+        console.log(lastEntity[0])
+
+        //Inserting bot response only if last input is from 'User'
+        if (lastEntity[0] === 'User') {
+            //Looping on Responses object.
+            for (let Reskey in Responses) {
+                // checking if input matches with key of respone object
+                if (chatHistory[Chatkey]['User'] === Reskey) {
+                    // console.log(Responses[Reskey])
+                    let ResponseVal = Responses[Reskey] //getting response messege for Reskey
+                    setTimeout(() => { setChatObject('Bot', ResponseVal) }, 1000) //delay to avoid overring value of existing datatime key chatHistoryObject.
+                }
+            }
+        }
+    }
+
+    return <>
+        <input type='text' onChange={setInput} value={inputVal}></input>
+        <button onClick={() => { setChatObject('User', inputVal) }}> send</button>
+    </>
 
 }
 
@@ -45,3 +92,4 @@ export default UserInputModule
  * task 6: design UI
  * 
  */
+
